84f24c542826c3b7f685ac28d4047809993560ab
PH <leopodinoraphael@gmail.com>
2026-03-23 23:19:17 -0300

    Add my ambient in
    # Conflicts:
    #	pom.xml

--- src/main/java/com/nexoart/nexocore/config/SecurityConfig.java
+++ src/main/java/com/nexoart/nexocore/config/SecurityConfig.java
@@ -6,15 +6,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/ping").permitAll()
-                        .anyRequest().authenticated());
-        return http.build();
-    }
+    @Configuration
+    public class SecurityConfig {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.csrf(csrf -> csrf.disable())
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/ping").permitAll()
+                            .anyRequest().authenticated());
+            return http.build();
+        }
 }
